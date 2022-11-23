96e80a61c899bb5d2bd0a3a2911c7823515be24b
Pratham_singh <pratham@teamvariance.com>
2022-11-23 15:45:03 +0530

    first commit

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+})
+export class DialogComponent {
+  freshnessList = ['Brand New', 'Second hand', 'Refusbished'];
+  productForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      freshness: ['', Validators.required],
+      price: ['', Validators.required],
+      comment: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+  }
+  addProduct() {
+    console.log(this.productForm.value);
+  }
+}
